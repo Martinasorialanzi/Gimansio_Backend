b5efcb03629c4b21997e0cb285b738418ed76500
LucasLon22 <crlucaslondero@gmail.com>
2023-01-30 19:35:11 -0300

    back planes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser"); //OK
 const express= require(`express`) //con esta libreria lo que hacemos es manejar todo lo que es hacer un servidor //OK
 // const productRoutes= require(`./routes/productRoutes`)
 const routesUsuario = require('./routes/usuario')//OK
+const routesPlanes = require('./routes/planes')//OK
 const app=express(); //OK
 
 
@@ -11,7 +12,8 @@ app.use(bodyParser.urlencoded({extended:'true'})) //esto lo que hace es parsear 
 app.use(bodyParser.json()); //y si llega a venir una respuesta en JSON la vamos a parsear de esta manera
 
 
-app.use(`/v1`,routesUsuario)  //poner nombre de la version de la api entonces el quedearia http://localhost:8080/v1/productos
+app.use(`/v1`, routesUsuario)  //poner nombre de la version de la api entonces el quedearia http://localhost:8080/v1/productos
+app.use(`/v2`, routesPlanes)
 
 module.exports=app;
 
