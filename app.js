37c0d441799491fdc7b47f56e561633d71df7b11
LucasLon22 <crlucaslondero@gmail.com>
2023-02-02 16:23:47 -0300

    prueba diego

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ const app=express(); //OK
 //    optionSuccessStatus:200,
 // }
 
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'Content-Type', 'Authorization');
+    next();
+})
 
 app.use(cors(corsOptions)) // Use this after the variable declaration
 
